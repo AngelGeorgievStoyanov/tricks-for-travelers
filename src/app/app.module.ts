0bceba223b5e1c4c60accf64bf3f41e06f1c074b
Angel <angelo_stoyanov@abv.bg>
2022-11-23 11:37:22 +0200

    login,register,profile-HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './components/shared/shared.module';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { UserModule } from './components/user/user.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
+    UserModule,
     AppRoutingModule
   ],
   providers: [],
