72aa5f753354aa436008d1fb356223f356985a96
Angel <angelo_stoyanov@abv.bg>
2022-11-23 16:31:48 +0200

    CSS Login Register IUser interface UserService

---
+++ src/app/components/shared/interfaces/user.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+    email: string;
+    _id:string;
+    __v: string;
+    accessToken:string;
+}
+
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { IUser } from "../components/shared/interfaces/user";
+import { tap } from 'rxjs/operators';
+
+
+
+const apiURL = environment.apiURL;
+
+@Injectable()
+export class UserService {
+
+    user: IUser | undefined = undefined;
+
+    get isLogged(): boolean {
+        return !!this.user;
+    }
+
+    constructor(private http: HttpClient) { }
+
+
+    register(data: { email: string; pasword: string }) {
+
+        return this.http.post<IUser>(`${apiURL}/users/register`, data, { withCredentials: false }).pipe(
+            tap((user) => this.user = user)
+        )
+
+
+    }
+
+    login(data: { email: string; password: string }) {
+
+        return this.http.post<IUser>(`${apiURL}/users/login`, data, { withCredentials: false }).pipe(
+            tap((user) => this.user = user)
+        );
+    }
+
+
+}
+
