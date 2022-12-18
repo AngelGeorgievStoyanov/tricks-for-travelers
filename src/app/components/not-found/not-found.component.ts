ece3d3a47d77f269ce9fea1b10d8b32b2c4932f1
Angel <angelo_stoyanov@abv.bg>
2022-12-18 11:57:55 +0200

    page not found

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as internal from 'stream';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
@@ -7,18 +8,40 @@ import { Router } from '@angular/router';
 })
 export class NotFoundComponent {
 
+  internal: any
+  internalSec: any
 
 
+  seconds: number
   constructor(private router: Router) {
+    this.seconds = 5
     this.fetchNotFound()
   }
 
 
   fetchNotFound(): void {
-    setTimeout(() => (
+
+
+
+    this.internalSec = setInterval(() => {
+      this.seconds = this.seconds - 1
+    }, 1000)
+
+    this.internal = setTimeout(() => (
       this.router.navigate(['/home'])
     ), 5000)
 
   }
+
+
+
+  goHome() {
+    // clearTimeout(this.internal)
+
+    clearInterval(this.internalSec)
+
+    this.router.navigate(['/home'])
+  }
+
 }
 
